c93f95443ced8506013b180bad1253d3c816c8bb
soundofautmn <qjm1412@163.com>
2026-03-11 18:24:38 +0800

    port

--- apps/gradio/motioncanvas.py
+++ apps/gradio/motioncanvas.py
@@ -345,4 +345,4 @@ with gr.Blocks(
             )
 
 if __name__ == "__main__":
-    app.launch(server_name="0.0.0.0", server_port=7860, share=False)
+    app.launch(server_name="0.0.0.0", server_port=6006, share=False)
